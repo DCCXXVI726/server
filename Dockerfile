ae6fc8b0bde08d0055cefb866a0d0af490ed4405
DCCXXVI726 <DCCXXVI726762@gmail.com>
2021-11-26 12:05:16 +0500

    add home pagfe

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM golang:latest
 RUN mkdir /app
 ADD . /app/
 WORKDIR /app
-RUN go build  ./cmd/apiserver
+RUN go build  -o apiserver ./src
 CMD ./apiserver
---
+++ src/main.go
@@ -0,0 +1,20 @@
+package src
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to homePage!")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", homePage)
+	bindAddr :=  os.Getenv("PORT")
+	if bindAddr == "" {
+		bindAddr = "8080"
+	}
+	http.ListenAndServe(":"+bindAddr, nil)
+}
