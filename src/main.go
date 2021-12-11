a389aa16e53b31a70f04872ab969cdcc66a702fc
DCCXXVI726 <DCCXXVI726762@gmail.com>
2021-12-11 07:19:04 +0500

    before refactoring main

--- src/main.go
+++ src/main.go
@@ -2,22 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
-	"log"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to homePage!")
 }
 
+func register(res http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		s, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			//ошибка
+		}
+
+	}
+}
+
 func handleRequest() {
 	http.HandleFunc("/", homePage)
-	bindAddr :=  os.Getenv("PORT")
+	http.HandleFunc("/register", register)
+	bindAddr := os.Getenv("PORT")
 	if bindAddr == "" {
 		bindAddr = "8081"
 	}
-	log.Fatal(http.ListenAndServe(":" + bindAddr, nil))
+	log.Fatal(http.ListenAndServe(":"+bindAddr, nil))
 }
 
 func main() {
