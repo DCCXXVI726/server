6a45805fb69c40c7ee072ccf1788c86668b20a39
DCCXXVI726 <DCCXXVI726762@gmail.com>
2021-12-21 17:23:56 +0500

    add store

--- src/apiserver.go
+++ src/apiserver.go
@@ -11,12 +11,14 @@ import (
 type ApiServer struct {
 	logger *logrus.Logger
 	router *mux.Router
+	store  *Store
 }
 
 func NewServer() *ApiServer {
 	return &ApiServer{
 		logger: logrus.New(),
 		router: mux.NewRouter(),
+		store:  NewStore(),
 	}
 }
 
@@ -31,6 +33,11 @@ func (s *ApiServer) Start() error {
 	}
 
 	s.configureRouting()
+
+	if err := s.configureStore(); err != nil {
+		return fmt.Errorf("can't configure store: %s", err)
+	}
+	defer s.store.Close()
 
 	s.logger.Info("starting server")
 	return http.ListenAndServe(":"+bindAddr, s.router)
@@ -53,10 +60,25 @@ func (s *ApiServer) configureLogger() error {
 
 func (s *ApiServer) configureRouting() {
 	s.router.HandleFunc("/", s.handleHome())
+	s.router.HandleFunc("/registration", s.handleRegistration())
+}
+
+func (s *ApiServer) configureStore() error {
+	if err := s.store.Open(); err != nil {
+		fmt.Errorf("can't open store: %s", err)
+	}
+	return nil
 }
 
 func (s *ApiServer) handleHome() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to homePage!")
+		_, user := s.store.all()
+		fmt.Fprintf(w, "Welcome to homePage!"+user)
+	}
+}
+
+func (s *ApiServer) handleRegistration() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
 	}
 }
---
+++ src/store.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"os"
+)
+
+type User struct {
+	Email string
+	Pass  string
+	ID    int
+}
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore() *Store {
+	return &Store{}
+}
+
+func (s *Store) all() (int, string) {
+	var (
+		id   int
+		user string
+	)
+	s.db.QueryRow("select user, id from users LIMIT 1").Scan(&id, &user)
+	return id, user
+}
+
+func (s *Store) Open() error {
+	databaseUrl := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", databaseUrl)
+	if err != nil {
+		return fmt.Errorf("can't open db: %s", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return fmt.Errorf("can't ping db: %s", err)
+	}
+
+	s.db = db
+	return nil
+}
+
+func (s *Store) Close() {
+	s.db.Close()
+}
